35bf86cf2b804bc23aa5971131a24728859401b9
racap <raulcapdevila@gmail.com>
2026-04-24 21:50:47 +0200

    estructura inicial del proyecto

---
+++ src/app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("🏠 Predictor de precios de vivienda")
+st.write("Aplicación desarrollada para AI Foundations — Fundació URV")
+
+st.info("Modelo en construcción... próximamente aquí la predicción!")
